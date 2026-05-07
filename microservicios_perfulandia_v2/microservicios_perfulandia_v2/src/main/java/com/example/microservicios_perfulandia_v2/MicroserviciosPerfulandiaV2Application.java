70b1cb5b9a249f0640c75c274e44f4143f8ceb00
Cristobal Viveros <cris.viveros@duocuc.cl>
2026-05-07 12:22:09 -0400

    Add files via upload

---
+++ microservicios_perfulandia_v2/microservicios_perfulandia_v2/src/main/java/com/example/microservicios_perfulandia_v2/MicroserviciosPerfulandiaV2Application.java
@@ -0,0 +1,13 @@
+package com.example.microservicios_perfulandia_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosPerfulandiaV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosPerfulandiaV2Application.class, args);
+	}
+
+}
---
+++ microservicios_perfulandia_v2/microservicios_perfulandia_v2/src/test/java/com/example/microservicios_perfulandia_v2/MicroserviciosPerfulandiaV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicios_perfulandia_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosPerfulandiaV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
